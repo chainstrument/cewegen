9e668c54016b7b9f96ca38b5be12a6b25d10f96f
Chainstrument <minitutoriel@gmail.com>
2024-09-12 07:05:15 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ function App() {
   
 
   return (
-    <> 
+    <div className='flex aligns-items-center flex-col'> 
         <h1 className='text-3xl underline font-bold'>{titlePrincipal}</h1>
         {experiences.map((experience) => {
-        return <Experience {...experience}/>
-          
-})}
-    </>
+                return <Experience {...experience} className="gap-4"/>
+                  
+        })}
+    </div>
   )
 }
 
